cb53cc6d879246583c2c03344d27a671b43f4e75
Rumesh Eranga Hapuarachchi <rehrumesh@hotmail.com>
2020-07-05 23:48:04 +0530

    Updated cats application

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+window.renderCats = (containerId, history) => {
+  ReactDOM.render(
+    <App history={history} />,
+    document.getElementById(containerId),
+  );
+  serviceWorker.unregister();
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+window.unmountCats = containerId => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
+
+if (!document.getElementById('Cats-container')) {
+  ReactDOM.render(<App />, document.getElementById('root'));
+  serviceWorker.unregister();
+}
